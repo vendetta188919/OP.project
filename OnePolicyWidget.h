fb309692fa419b27a37d6a838e8983ddf14636d6
Alukarddd <104027772+Alukarddd@users.noreply.github.com>
2024-05-29 19:26:37 +0300

    change Policy

--- OnePolicyWidget.h
+++ OnePolicyWidget.h
@@ -12,6 +12,9 @@ class OnePolicyWidget : public QDialog
 public:
     explicit OnePolicyWidget(QWidget* pParent = nullptr);
     void makeDescription(const QVariant& crVar, const QString& crQstrTxt);
+    QLabel* makeDescriptionLbl(const QString& crQstrTxt) const;
+
+void AdjustHelpMenu();
 
 private:
     QHBoxLayout* makePairWidgets(QWidget* pLeftWidget, QWidget* pRightWidget) const;
